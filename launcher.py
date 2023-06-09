9476e7c2cb7735079c64a62b8cfe139fb3ccccb0
Bobloblaw234 <98914439+Bobloblaw234@users.noreply.github.com>
2023-06-09 12:48:11 -0500

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,11 +1,8 @@
-import os
 import sys
-import shutil
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
 import subprocess
-import urllib.request
 
 class Launcher(QWidget):
     def __init__(self):
@@ -31,54 +28,32 @@ class Launcher(QWidget):
         self.title.setStyleSheet("color: white; font-size: 20px; margin-bottom: 20px;")
         self.layout.addWidget(self.title)
 
-        self.button1 = QPushButton('Run SALUTE')
+        self.button1 = QPushButton('SALUTE Reports')
         self.button1.setStyleSheet("background-color: rgba(255, 0, 0, 15%); border: 1px solid white; color: white;")
         self.button1.clicked.connect(self.run_app1)
         self.layout.addWidget(self.button1)
 
-        self.button2 = QPushButton('Run App 2')
+        self.button2 = QPushButton('Export Reports')
         self.button2.setStyleSheet("background-color: rgba(0, 0, 255, 15%); border: 1px solid white; color: white;")
         self.button2.clicked.connect(self.run_app2)
         self.layout.addWidget(self.button2)
 
-        self.button3 = QPushButton('Run App 3')
+        self.button3 = QPushButton('Upload Images')
         self.button3.setStyleSheet("background-color: rgba(0, 255, 0, 15%); border: 1px solid white; color: white;")
         self.button3.clicked.connect(self.run_app3)
         self.layout.addWidget(self.button3)
 
     def run_app1(self):
-        filename = os.path.join(os.path.expanduser("~/Documents/Downloads"), "SALUTE.py")
-        if not os.path.exists(filename):
-            url = "https://raw.githubusercontent.com/Bobloblaw234/SALUTE/main/SALUTE.py"
-            self.download_file(url, filename)
-        self.run_app(filename)
+        subprocess.Popen(['cmd', '/C', 'start', 'cmd', '/K',
+                          r'python -m streamlit run SALUTE.py'])
 
     def run_app2(self):
-        filename = os.path.join(os.path.expanduser("~/Documents/Downloads"), "export_SALUTE.py")
-        if not os.path.exists(filename):
-            url = "https://raw.githubusercontent.com/Bobloblaw234/SALUTE/main/export_SALUTE.py"
-            self.download_file(url, filename)
-        self.run_app(filename)
+        subprocess.Popen(['cmd', '/C', 'start', 'cmd', '/K',
+                          r'python -m streamlit run export_SALUTE.py'])
 
     def run_app3(self):
-        filename = os.path.join(os.path.expanduser("~/Documents/Downloads"), "upload_images.py")
-        if not os.path.exists(filename):
-            url = "https://raw.githubusercontent.com/Bobloblaw234/SALUTE/main/upload_images.py"
-            self.download_file(url, filename)
-        self.run_app(filename)
-
-    def download_file(self, url, filename):
-        try:
-            urllib.request.urlretrieve(url, filename)
-        except Exception as e:
-            print(f"Error downloading file: {e}")
-
-    def run_app(self, filename):
-        if sys.platform == "win32":
-            os.startfile(filename)
-        else:
-            opener = "open" if sys.platform == "darwin" else "xdg-open"
-            subprocess.call([opener, filename])
+        subprocess.Popen(['cmd', '/C', 'start', 'cmd', '/K',
+                          r'python -m streamlit run upload_images.py'])
 
 
 def main():
